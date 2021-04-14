632bc2498910bdd561ca61c0ee561d567e10bf72
unknown <mindy80230@gmail.com>
2021-04-14 12:26:40 +0800

    登出無法使用

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
+
+use App\Http\Controllers\AuthController;
 use App\Models\Product;
 use App\Http\Controllers\ProductController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('products',ProductController::class);
 
 // Public routes
+Route::post('/register',[AuthController::class,'register']);
 Route::get('/products', [ProductController::class,'index']);
 Route::get('/products/{id}', [ProductController::class,'show']);
 Route::get('/products/search/{name}',[ProductController::class,'search']);
@@ -29,6 +33,7 @@ Route::group(['middleware' => ['auth:sanctum']],function () {
     Route::post('/products',[ProductController::class,'store']);
     Route::put('/products/{id}',[ProductController::class,'update']);
     Route::delete('/products/{id}',[ProductController::class,'destroy']);
+    Route::post('/logout',[AuthController::class,'logout']);
 });
 
 
